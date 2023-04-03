2872c31a5c7f3e8bd33ad8de7794470d8fcfab20
1220505053 <127299631+1220505053@users.noreply.github.com>
2023-04-03 20:44:50 +0300

    Add files via upload

---
+++ DallarinToplamiKokMu.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+class Agac    // Burada kok ve dugumleri tanitiyoruz
+{
+    public int Deger;
+    public Agac SolDugum;
+    public Agac SagDugum;
+    
+    public Agac(int deger)  //Agac tanimlioyruz
+    {
+        Deger = deger;
+    }
+}
+
+class Program
+{
+    static bool ToplamKontrol(Agac dugum)   
+    {
+        if (dugum == null) //Gecersiz degereler icin cikti yazdiriyoruz 
+        {
+            Console.WriteLine("Dikkat: null değerli bir düğümle karşılaşıldı!");
+            return false;
+        }
+
+        else if (dugum.SolDugum == null && dugum.SagDugum == null)
+        {
+            // Düğümün çocukları yoksa, düğümün kendisi toplamını temsil eder
+            return true;
+        }
+        else
+        {
+            // Çocukları olan düğümün alt düğümlerinin toplamını buluyoruz
+            int altToplam = 0;
+            if (dugum.SolDugum != null)
+            {
+                altToplam += dugum.SolDugum.Deger;
+            }
+            if (dugum.SagDugum != null)
+            {
+                altToplam += dugum.SagDugum.Deger;
+            }
+
+            // Alt düğümlerin toplamı düğüm değerine eşit mi diye kontrol ediyoruz
+            return dugum.Deger == altToplam && ToplamKontrol(dugum.SolDugum) && ToplamKontrol(dugum.SagDugum);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // Bir örnek ikili ağaç oluşturalım
+        Agac kok = new Agac(12)
+        {
+            SolDugum = new Agac(5),
+            SagDugum = new Agac(7)
+        };
+
+        // Toplamı kontrol edelim
+        bool sonuc = ToplamKontrol(kok);
+
+        // Sonucu ekrana yazdıralım
+        if (sonuc)
+        {
+            Console.WriteLine("Kok dugumun altindaki dugumlerin toplamidir"); 
+        }
+        else
+        {
+            Console.WriteLine("Kok dugumun altindaki dugumlerin toplami degildir.");
+        }
+
+        
+        Console.ReadKey();
+    }
+    }
+
+  
---
+++ README.txt
@@ -0,0 +1,17 @@
+C# dilinde size 2 düğümden oluşan ikili bir ağacın kökü verildiğinde, bu değerin alt düğümlerin(child) toplamı olup olmadığını bulan programdir. Binarytreelerin alt dallari koke esitligini tespit etmekte kullanilabilir.
+
+
+Indirdigimiz dosyalari calistirip ustunde degisiklikler yapmak icin C# diline entegre bir uygulamaya ihtiyacimiz var. Benim onerim Visual Studio olacak.
+Visual Studio' yu ucretsiz bir sekilde Micrososft' un web sitesinden indirebilirsiniz.
+
+Visual Studio 2022 indirmek icin asagidaki linki kullanabilirsiniz:
+https://visualstudio.microsoft.com/downloads/
+
+ Visual Studio kurulumu bittikten sonra .cs dosyalarina su sirayla erisebilirisniz:
+ 1- Visual Studio' yu acin ve "File -> New -> Project" sirasini takip edin
+ 2- "Console App" basligini listeden secin ve isimlendirin.
+ 3- Proje olusturulduktan sonra "Program.cs" seklinde isimlendirildigini goreceksiniz. Bu kutu altinda yazilan kodlarin islevsel bir sekilde
+ bir arada durmasini sagliyor.
+ 4- Sayfanin en ust kismindaki Visual Studio ikonunun yanindaki "File -> Open -> File -> "Yukledigimiz Dosya" -> Open" yaparak doyayi aciyoruz.
+ 5- F5 tusuna basarak direkt programi calistirabilirsinz
+ NOT: Kodlardaki yesil yazilari okuyarak kodlarin islevini ogrenebilir ve istediginiz gibi degistirebilirsiniz
